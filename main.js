7de84326327fc78261e3fe1bf8f1d406b14998e1
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2017-10-09 16:54:11 +0100

    enable devDeps

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const RouterWrapper = require('./RouterWrapper');
 const DatabaseLayer = require('./DBLayer');
 const Server = require('./Server');
 
+
+// Launch server
 let server = new Server(express, new RouterWrapper(express.Router()), new DatabaseLayer());
 
 
