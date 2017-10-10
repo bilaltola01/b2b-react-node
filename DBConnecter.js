54188993d2f33989e926c30c56f629ca2a36ee48
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2017-10-10 17:14:00 +0100

    change knex opts

--- DBConnecter.js
+++ DBConnecter.js
@@ -6,14 +6,13 @@ let DBConnecter = {
 		return knex({
 		 	client: constants.DB_TYPE,
 		 	debug: true,
-			connection: (constants.HOST === 'localhost') ? {
+			connection: {
 		    	host: host || constants.DB_URL,
 		    	port: port || constants.DB_PORT,
 		    	user: user || constants.DB_USER,
 		    	password: pwd || constants.DB_PWD,
-		    	database: db || constants.DB_NAME,
-		    	ssl: has_ssl || constants.DB_SSL_ENABLED
-			} : constants.DB_STRING,
+		    	database: db || constants.DB_NAME
+			},
 			ssl: has_ssl,
 			pool: {
 				max: 100
