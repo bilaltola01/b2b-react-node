c66f5c34d36457bf56392b91855917ec86b5ede3
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2017-10-10 14:27:38 +0100

    Update env vars and launch locally now via npm start:local

--- constants.js
+++ constants.js
@@ -1,12 +1,12 @@
 // Move this out and put in ENV vars
-exports.HOST = 'localhost';
+exports.HOST = process.env.HOST;
 exports.APP_PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = 'Q~&NvAHug*`3%M=T';
-exports.CLOUDINARY_NAME  = 'one-menu';
-exports.CLOUDINARY_SECRET = 'VNjS8wLJW1x23xH_Sz364J4xfew';
-exports.CLOUDINARY_API_KEY = '834776295221634';
-exports.DB_URL = /*'10.16.11.13';*/'localhost'; //46.32.240.33
-exports.DB_ENV = 'DEV';
-exports.DB_NAME = 'oneme-5ru-u-121572';
-exports.DB_USER = /*'oneme-5ru-u-121572';*/'root';
-exports.DB_PWD = /*'wTYTMR^E!';*/'root';
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.CLOUDINARY_NAME = process.env.CLOUDINARY_NAME;
+exports.CLOUDINARY_SECRET = process.env.CLOUDINARY_SECRET;
+exports.CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+exports.DB_URL = /*'10.16.11.13';*/process.env.DB_URL; //46.32.240.33
+exports.DB_ENV = process.env.DB_ENV;
+exports.DB_NAME = process.env.DB_NAME;
+exports.DB_USER = /*'oneme-5ru-u-121572';*/process.env.DB_USER;
+exports.DB_PWD = /*'wTYTMR^E!';*/process.env.DB_PWD;
