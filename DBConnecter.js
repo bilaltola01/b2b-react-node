1546168ef823940a36a11e51269167976698ba94
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2017-10-10 16:26:58 +0100

    chane connection method in knex

--- DBConnecter.js
+++ DBConnecter.js
@@ -2,14 +2,16 @@ const knex = require('knex');
 const constants = require('./constants');
 
 let DBConnecter = {
-	connect: (host, db, user, pwd) => {
+	connect: (host, port, db, user, pwd, has_ssl) => {
 		return knex({
 		 	client: constants.DB_TYPE,
 			connection: {
 		    	host: host || constants.DB_URL,
+		    	port: port || constants.DB_PORT,
 		    	user: user || constants.DB_USER,
 		    	password: pwd || constants.DB_PWD,
-		    	database: db || constants.DB_NAME
+		    	database: db || constants.DB_NAME,
+		    	ssl: has_ssl || constants.DB_SSL_ENABLED
 			}
 		});
 	}
