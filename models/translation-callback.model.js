fc013eeba4092d1bd2c4680b2d06e49b231e46ca
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2017-10-16 10:36:04 +0100

    implemented generic way of handling straker uri callbacks

---
+++ models/translation-callback.model.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const DBLayer = require('../DBLayer');
+const db = DBLayer.connection;
+const dateUtils = require('../shared/date-utils');
+
+const MenuTranslation = require('./menu-translation.model');
+const MealTranslation = require('./meal-translation.model');
+const MenuCategoryTranslation = require('./menu-category-translation.model');
+
+// Update translation status in the database
+// Returns a resolved Promise containing its id
+let TranslationCallback = class {
+
+};
+
+TranslationCallback.create = (query, obj) => {
+  let job = obj[0];
+  let type = query ? query.type : '';
+
+  console.log(job);
+  console.log(type);
+
+  return update(type, job);
+};
+
+function update (type, job) {
+  switch (type) {
+    case 'meal':
+      return updateObjectTranslation(MealTranslation, 'MealTranslationID',job);
+    case 'menucategory':
+      return updateObjectTranslation(MenuCategoryTranslation, 'MenuCategoryTranslationID', job);
+    case 'menu':
+      return updateObjectTranslation(MenuTranslation, 'MenuTranslationID', job);
+    default:
+      return updateObjectTranslation(MenuTranslation, 'MenuTranslationID', job);
+  }
+}
+
+function updateObjectTranslation (component, idStr, job) {
+  return component.get({
+    JobNumber: job.job_key
+  }).then(translation => {
+    return updateTranslation(translation[0], component, idStr, job);
+  }).catch((err) => {
+    console.error(err);
+  });
+}
+
+function updateTranslation (translation, component, idStr, job) {
+  // If successful, update the db entry with the translation PENDING
+  let id = translation[idStr];
+  let dbObj = {
+    Status: job.status,
+    JobNumber: job.job_key
+  };
+
+  if (job.wordcount) {
+    dbObj.WordCount = job.wordcount;
+  }
+
+  if (job.translation_type === 'text') {
+    if (job.translated_text && job.translated_text.length > 0 && job.translated_text.download_url) {
+      dbObj.Url = job.translated_text.download_url;
+      dbObj.Text = job.translated_text.download_text;
+    }
+  }
+
+  if (job.translation_type === 'file') {
+    if (job.translated_file && job.translated_file.length > 0 && job.translated_file.download_url) {
+      dbObj.Url = job.translated_file.download_url;
+    }
+  }
+
+  return component.update(id, dbObj);
+}
+
+module.exports = TranslationCallback;
