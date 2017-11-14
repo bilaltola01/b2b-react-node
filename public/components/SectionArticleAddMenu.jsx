8dae9ea504d01eaea650646e0753c4047aca4630
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2017-11-14 18:56:13 +0000

    add error message if add menu before branch

--- public/components/SectionArticleAddMenu.jsx
+++ public/components/SectionArticleAddMenu.jsx
@@ -54,6 +54,8 @@ class SectionArticleAddMenu extends Component {
 
 		console.log(this.props);
 
+		const profileBranches = this.props.profile.branches;
+
 		const menuBranches = (
 			<MenuBranchesEdit branches={branches} onChange={this.handlers.onChanges} />
 		);
@@ -66,6 +68,48 @@ class SectionArticleAddMenu extends Component {
 			<MenuCategoriesEdit categories={categories} onChange={this.handlers.onChanges} />
 		);
 
+		const addMenuComponent = (profileBranches && profileBranches.length > 0) ? (
+			<div className="content--container global-padding-wrapper no-border-top">
+                <form id="form-menu-content" className="content--edit">
+                    <div className="edit--block">
+                        <label className="label--edit">Enter new Title:</label>
+                        <input className="input--edit" type="text" name="menu--price" id="menu-title" defaultValue={"New title..."} onChange={(e) => this.handlers.onChanges('main', {target: e, key: 'title'})} />
+                    </div>
+                    <div className="edit--block">
+                        <label className="label--edit">Enter new Description:</label>
+                        <input className="input--edit" type="text" name="menu--description" id="menu-description" defaultValue={"New description..."} onChange={(e) => this.handlers.onChanges('main', {target: e, key: 'description'})} />
+                    </div>
+                    <div className="edit--block">
+                        <label className="label--edit">Enter new Price:</label>
+                        <input className="input--edit" type="text" name="menu--price" id="menu-price" defaultValue={"New price..."} onChange={(e) => this.handlers.onChanges('main', {target: e, key: 'price'})} />
+                    </div>
+                </form>
+
+                <div className="menu--languages">
+	                {menuBranches}
+	            </div>
+
+                <div className="menu--languages">
+	                {menuLanguages}
+	            </div>
+
+	            <div className="menu--categories">
+	                {menuCategories}
+	            </div>
+            </div>
+		) : (
+			<div className="global-padding-wrapper branches-container">
+				<h2 className="no-items--headline">It looks like you have not entered any branches yet. You must add a branch first before entering any menus.</h2>
+				<div className="branch--add">
+	            	<Link to="/branch/add/1" >
+						<div className="add-item dashed">
+							<span>Add a Branch <strong>+</strong></span>
+						</div>
+					</Link>
+	            </div>
+            </div>
+		);
+
 		return (
 			<div>
 	            <div className="content--container global-padding-wrapper">
@@ -74,34 +118,7 @@ class SectionArticleAddMenu extends Component {
 	                </h2>
 	            </div>
 
-	            <div className="content--container global-padding-wrapper no-border-top">
-	                <form id="form-menu-content" className="content--edit">
-	                    <div className="edit--block">
-	                        <label className="label--edit">Enter new Title:</label>
-	                        <input className="input--edit" type="text" name="menu--price" id="menu-title" defaultValue={"New title..."} onChange={(e) => this.handlers.onChanges('main', {target: e, key: 'title'})} />
-	                    </div>
-	                    <div className="edit--block">
-	                        <label className="label--edit">Enter new Description:</label>
-	                        <input className="input--edit" type="text" name="menu--description" id="menu-description" defaultValue={"New description..."} onChange={(e) => this.handlers.onChanges('main', {target: e, key: 'description'})} />
-	                    </div>
-	                    <div className="edit--block">
-	                        <label className="label--edit">Enter new Price:</label>
-	                        <input className="input--edit" type="text" name="menu--price" id="menu-price" defaultValue={"New price..."} onChange={(e) => this.handlers.onChanges('main', {target: e, key: 'price'})} />
-	                    </div>
-	                </form>
-
-	                <div className="menu--languages">
-		                {menuBranches}
-		            </div>
-
-	                <div className="menu--languages">
-		                {menuLanguages}
-		            </div>
-
-		            <div className="menu--categories">
-		                {menuCategories}
-		            </div>
-	            </div>
+	            {addMenuComponent}
 			</div>
 		)
 	}
