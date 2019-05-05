21bdf7b3969653e13e1d85ac67a5d661a0b9f704
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-05-06 01:38:59 +0800

    Add API endpoints
    - get digital-menu plan
    - post charge digital-menu plan

--- controllers/stripe.controller.js
+++ controllers/stripe.controller.js
@@ -1,24 +1,64 @@
 'use strict';
 const constants = require('../constants');
 const stripe = require("stripe")(constants.STRIPE_SECRET_KEY);
+const Company = require('../models/company.model');
 
 class StripeController {
 
 }
 
-StripeController.post("/charge", async (req, res) => {
-    try {
-        let {status} = await stripe.charges.create({
-            amount: 2000,
-            currency: "usd",
-            description: "An example charge",
-            source: req.body
-        });
+StripeController.chargeDigitalMenuPlan = async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
 
-        res.json({status});
-    } catch (err) {
-        res.status(500).end();
-    }
-});
+    const data = JSON.parse(JSON.stringify(req.body));
+
+    const email = data.email;
+    const token = data.stripeToken;
+    Company.customerIDExists(email).then(customerIDExists => {
+        if (!customerIDExists) {
+            stripe.customers.create({
+                    description: 'Customer for ' + email,
+                    source: token,
+                    email: email
+                }, function(err, customer) {
+                    if (err) {
+                        res.status(204).send({ success: false, message: 'stripe create customer failed', obj: err });
+                    }
+                    Company.getByEmail(email).then(company => {
+                        company.CustomerID = customer.id;
+                        Company.update(company.CompanyID, company);
+                    })
+                }
+            );
+        }
+        Company.getByEmail(email).then(company => {
+            stripe.subscriptions.create({
+                customer: company.CustomerID,
+                items: [
+                    {
+                        plan: 'digital-menu',
+                    },
+                ]
+                }, function(err, subscription) {
+                    if (err) {
+                        res.status(204).send({ success: false, message: 'digital menu plan failed', obj: err });
+                    }
+                    res.status(200).json({ success: true, message: 'digital menu plan successfully activated', obj: subscription });
+                }
+            ); 
+        })
+        
+    })
+};
+
+StripeController.getDigitalMenuPlan = async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    stripe.plans.retrieve('digital-menu', function(err, plan) {
+        if (err) {
+            res.status(204).send({ success: false, message: 'digital menu plan get failed', obj: err });
+        }
+        res.status(200).json({ success: true, message: 'digital menu plan successfully fetched', obj: plan });
+    });
+}
 
 module.exports = StripeController;
